147dd1e42dcf6aec5a848463b9595a4210588865
Ali Zein Al Dine <alizeinaldine@Alis-MacBook-Pro.local>
2023-10-28 23:45:12 +0300

    backend setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+require('dotenv').config();
+
+const HOSTNAME = process.env.BACKEND_HOSTNAME;
+const PORT = process.env.BACKEND_PORT;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any());
+
+app.listen(PORT, HOSTNAME, () => {
+  console.log(`Backend is running on http://${HOSTNAME}:${PORT}`);
+});
+
+app.get('/', (_, response) => {
+  return response.send('<h1>Hello World</h1>');
+});
+  
+const router = express.Router();
+
+module.exports = { router };
